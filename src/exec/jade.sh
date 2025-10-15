01000ea774ab1733d9754dfc0c78fdefbed0bbf9
joericks1998 <joericks1998@gmail.com>
2025-10-15 17:14:37 -0600

    Update jade.sh

--- src/exec/jade.sh
+++ src/exec/jade.sh
@@ -1,4 +1,4 @@
-function jade() {
-    echo "Jade is a programming language with LLM based interpretation."
-    python3 compile.py
-}
+#!/bin/bash
+
+echo "Jade is a programming language with LLM based interpretation."
+python3 compile.py
