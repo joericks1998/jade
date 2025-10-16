5d55ff5f170948cfae865ebff14d55f7aa9f1b3e
joericks1998 <joericks1998@gmail.com>
2025-10-15 19:48:17 -0600

    Update jadeinstall.sh

--- src/exec/jadeinstall.sh
+++ src/exec/jadeinstall.sh
@@ -12,10 +12,15 @@ fi
 
 # Check if /usr/local/bin directory exists
 if [ -d "/usr/local/bin" ]; then
-    curl -sSL https://raw.githubusercontent.com/joericks1998/jade/refs/heads/main/src/exec/jade.sh -o /usr/local/bin/jade
-    chmod +x /usr/local/bin/jade
+    sudo curl -sSL https://raw.githubusercontent.com/joericks1998/jade/refs/heads/main/src/exec/jade.sh -o /usr/local/bin/jade
+    sudo chmod +x /usr/local/bin/jade
     echo "jade installed successfully"
 else
     echo "Error: /usr/local/bin directory does not exist. Cannot set execute permissions on jade."
     exit 1
+fi
+
+if grep -q '404: Not Found' /usr/local/bin/jade; then
+  echo "Error: Download failed, got 404 Not Found."
+  exit 1
 fi
