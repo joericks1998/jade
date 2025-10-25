edbb362869b87d4c5bb2b8f2cc0f1fac25f9d123
Joe Ricks <joericks@Joes-Mac-mini.local>
2025-10-25 12:58:41 -0600

    made some architecture changes...

--- src/exec/jade.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-# Usage: jade.sh <file-to-compile>
-
-# Change to the directory where compile.py lives, or use full path
-python3 -m jade_project_JOERICKS1998.main
-
-echo "testing complete"
